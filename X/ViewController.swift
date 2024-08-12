93872cbe697f3763a4944dbf0d7e952fa188d281
Mabast O. Hamadamin <mabastu@gmail.com>
2024-08-12 12:33:08 +0300

    Initial Commit

---
+++ X/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  X
+//
+//  Created by Mabast on 2024-08-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
